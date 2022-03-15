815755aa9755104328dbcd27456a0fcbd2e347d2
Yashika <96520667+k-Yashika@users.noreply.github.com>
2022-03-15 23:03:22 +0800

    -

---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+  ini_set('display_errors', '1');
+  ini_set('display_startup_errors', '1');
+  error_reporting(E_ALL);
+	include('server.php');
+?>
+
+<?php
+		if(isset($_SESSION['success']) && $_SESSION['success'] == 1){
+	?>
+			<div class="alarm">
+				Your are login in site.
+				<a href="logout.php">
+					Log Out
+				</a>
+			</div>
+	<?php
+		}
+		else {
+	?>
+		<div class="header">
+			<h2>Login</h2>
+		</div>
+			
+		<form method="post" action="login.php">
+			<?php include('errors.php'); ?>
+			<div class="input-group">
+				<label>Username</label>
+				<input type="text" name="username" >
+			</div>
+			<div class="input-group">
+				<label>Password</label>
+				<input type="password" name="password">
+			</div>
+			<div class="input-group">
+				<button type="submit" class="submit" name="login_user">Login</button>
+			</div>
+			<p>
+				Not yet a member? <a href="signup.php">Sign up</a>
+			</p>
+		</form>
+	<?php
+		}
+	?>
